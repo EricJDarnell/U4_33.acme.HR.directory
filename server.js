fc644b891ce1c3c532a490e332b09ef7d4fe7895
EricJDarnell <eric.j.darnell@gmail.com>
2024-12-07 18:30:19 -0500

    api routes tested and tweaked to function

--- server.js
+++ server.js
@@ -1,13 +1,98 @@
-const pg = require("pg");
+import express from "express";
+import pg from "pg";
+
 const client = new pg.Client(
   process.env.DATABASE_URL || "postgres://localhost/acme_hr_directory_db"
 );
-const express = require("express");
 const app = express();
-app.use(express.json());
-app.use(require('morgan')('dev'));
+const port = process.env.PORT || 3000;
 
-const init = async () => {
+app.use(express.json());
+//api routes here ----------- 
+//get departments
+app.get(`/api/departments`, async (req, res, next) => {
+    try {
+        const SQL = `
+          select * from departments;
+        `;
+        const response = await client.query(SQL);
+        res.send(response.rows);
+    } catch (ex) {
+        next(ex);
+    }
+});
+//get employees
+app.get(`/api/employees`, async (req, res, next) => {
+    try {
+        const SQL = `
+          select * from employees;
+        `;
+        const response = await client.query(SQL);
+        res.send(response.rows);
+    } catch (ex) {
+        next(ex);
+    }
+});
+//post a department
+app.post('/api/departments', async (req, res, next) => {
+    try {
+        const SQL = `
+          insert into departments(name) 
+          values ($1)
+          returning *
+        `;
+    } catch (ex) {
+        next(ex);
+    }
+});
+//post an employee
+app.post(`/api/employees`, async (req, res, next) => {
+    try {
+        const SQL = `
+          insert into employees(name, department_id) 
+          values ($1, $2) 
+          returning *
+        `;
+        const response = await client.query(SQL, [
+            req.body.name,
+            req.body.department_id
+        ])
+        res.send(response.rows[0]);
+    } catch (ex) {
+        next(ex);
+    }
+});
+//put update an employee
+app.put(`/api/employees/:id`, async (req, res, next) => {
+    try {
+        const SQL = `
+            update employees
+            set name=$1, department_id=$2, updated_at=now()
+            where id=$3 returning *
+        `;
+        const response = await client.query(SQL, [
+            req.body.name,
+            req.body.department_id,
+            req.params.id
+        ]);
+        res.send(response.rows[0]);
+    } catch (ex) {
+        next(ex);
+    }
+});
+// delete some employees
+app.delete(`/api/employees/:id`, async (req, res, next) => {
+    try {
+        const SQL = `
+          delete from employees where id=$1;
+        `;
+        const response = await client.query(SQL, [req.params.id]);
+        res.sendStatus(204);
+    } catch (ex) {
+        next(ex);
+    }
+});
+async function init() {
   await client.connect();
   console.log("connected to database");
   let SQL = `
@@ -22,22 +107,22 @@ const init = async () => {
           name varchar(50) not null,
           created_at timestamp default now(),
           updated_at timestamp default now(),
-          department_id integer reference department(id) not null,
+          department_id integer references departments(id) not null
         );
     `;
     await client.query(SQL);
   console.log(`tables created`);
   SQL = `
-      insert into departments(name) values(Holders of Esoteric Knowledge);
-      insert into departments(name) values(Cool Guy Department);
-      insert into departments(name) values(Rad Fella Group);
-      insert into employees(name, department_id) values(Wolfy, (select id from departments(id) where name='Holders of Esoteric Knowledge'));
-      insert into employees(name, department_id) values(Ryan Peterson, (select id from departments(id) where name='Cool Guy Department'));
-      insert into employees(name, department_id) values(Karl, (select id from departments(id) where name='Rad Fella Group'));
+      insert into departments(name) values('Holders of Esoteric Knowledge');
+      insert into departments(name) values('Cool Guy Department');
+      insert into departments(name) values('Rad Fella Group');
+      insert into employees(name, department_id) values('Wolfy', (select id from departments where name='Holders of Esoteric Knowledge'));
+      insert into employees(name, department_id) values('Ryan Peterson', (select id from departments where name='Cool Guy Department'));
+      insert into employees(name, department_id) values('Karl', (select id from departments where name='Rad Fella Group'));
     `;
   await client.query(SQL);
   console.log("data seeded");
-  const port = process.env.PORT || 3000;
+
   app.listen(port, () => console.log(`listening on port ${port}`));
 };
 init();
