cef255ef924ed7d49975ab863272e6f5e312878b
EricJDarnell <eric.j.darnell@gmail.com>
2024-12-07 14:45:50 -0500

    rebuilding initialization steps

--- server.js
+++ server.js
@@ -1,8 +1,16 @@
-const pg = require('pg');
-const client = new pg.Client(process.env.DATABASE_URL || 'postgres://localhost/acme_hr_directory_db');
-async function init() {
-    await client.connect();
-    let SQL = `
+const pg = require("pg");
+const client = new pg.Client(
+  process.env.DATABASE_URL || "postgres://localhost/acme_hr_directory_db"
+);
+const express = require("express");
+const app = express();
+app.use(express.json());
+app.use(require('morgan')('dev'));
+
+const init = async () => {
+  await client.connect();
+  console.log("connected to database");
+  let SQL = `
         drop table if exists employees;
         drop table if exists departments;
         create table departments(
@@ -18,8 +26,8 @@ async function init() {
         );
     `;
     await client.query(SQL);
-    console.log(`tables created`);
-    SQL = `
+  console.log(`tables created`);
+  SQL = `
       insert into departments(name) values(Holders of Esoteric Knowledge);
       insert into departments(name) values(Cool Guy Department);
       insert into departments(name) values(Rad Fella Group);
@@ -27,7 +35,9 @@ async function init() {
       insert into employees(name, department_id) values(Ryan Peterson, (select id from departments(id) where name='Cool Guy Department'));
       insert into employees(name, department_id) values(Karl, (select id from departments(id) where name='Rad Fella Group'));
     `;
-    await client.query(SQL);
-    console.log('data seeded');
-}
+  await client.query(SQL);
+  console.log("data seeded");
+  const port = process.env.PORT || 3000;
+  app.listen(port, () => console.log(`listening on port ${port}`));
+};
 init();
